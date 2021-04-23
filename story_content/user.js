464b6009c3b198e074b0ccc8ad94994feaecaa7e
aoropio <andrewmark.a.oropio@smretail.com>
2021-04-23 09:36:32 +0800

    The SM Store User Guide Taglish Version
    Taglish Version User Guide

---
+++ story_content/user.js
@@ -0,0 +1,89 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6kRTqVQ9RXJ":
+        Script1();
+        break;
+      case "6RVggeJQdPs":
+        Script2();
+        break;
+      case "5kQ7g452gUW":
+        Script3();
+        break;
+      case "67d5MyLvtVJ":
+        Script4();
+        break;
+      case "6ZI8xn0ygzA":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+    var line = document.createElement("audio");
+    var head=document.getElementsByTagName('body')[0];
+    line.type = "audio/mp3";
+    line.src="";
+    line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = true;
+    head.appendChild(line);
+}
+
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location+"1.mp3";
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location + "1.mp3";
+audio.load();
+audio.play();
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location + "1.mp3";
+audio.load();
+audio.play();
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location + "1.mp3";
+audio.load();
+audio.play();
+}
+
